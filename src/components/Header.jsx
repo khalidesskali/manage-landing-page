05f4807fc4c883a6e7c21b7cfe2e9a2b319b6ff5
khalidesskali <esskalikhalid7@gmail.com>
2025-04-02 15:16:05 +0000

    fix responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
   const [isOpen, setIsopen] = useState(false);
 
   return (
-    <header className="relative h-20 flex">
-      <div className="container mx-auto px-4">
-        {/* Mobile version */}
+    <header className="relative h-20 flex z-100">
+      {/* Mobile version */}
+      <div className="container mx-auto px-4 lg:hidden">
         <div className="flex items-center justify-between h-full">
           <img src={logo} alt="Logo" loading="lazy" width={120} />
           <button className="z-100" onClick={() => setIsopen(!isOpen)}>
@@ -26,8 +26,8 @@ const Header = () => {
         <nav
           className={`${
             isOpen ? "flex" : "hidden"
-          } items-center flex-col gap-4 rounded shadow-darkgrayish-blue duration-300 
-           transition-all absolute left-1/2 -translate-x-1/2 2 p-10 bg-white 
+          } items-center flex-col gap-4 rounded shadow-darkgrayish-blue duration-300
+           transition-all absolute left-1/2 -translate-x-1/2 2 p-10 bg-white
            starting:opacity-0 starting:translate-y-2.5 z-100 w-[95vw]`}
         >
           <a href="#">Pricing</a>
@@ -36,6 +36,35 @@ const Header = () => {
           <a href="#">Carrers</a>
           <a href="#">Community</a>
         </nav>
+      </div>
+      {/* Laptop version */}
+      <div className="container mx-auto px-4 hidden lg:block">
+        <div className="flex items-center justify-between h-full">
+          <img src={logo} alt="Logo" loading="lazy" width={120} />
+          <nav className="flex items-center gap-10 text-sm">
+            <a href="#" className="text-primary-dark-blue font-medium">
+              Pricing
+            </a>
+            <a href="#" className="text-primary-dark-blue ">
+              Product
+            </a>
+            <a href="#" className="text-primary-dark-blue ">
+              About Us
+            </a>
+            <a href="#" className="text-primary-dark-blue ">
+              Carrers
+            </a>
+            <a href="#" className="text-primary-dark-blue ">
+              Community
+            </a>
+          </nav>
+          <button
+            className="block py-3 px-8 bg-primary-red text-white rounded-full 
+            cursor-pointer text-sm shadow-lg shadow-very-pale-red"
+          >
+            Get Started
+          </button>
+        </div>
       </div>
     </header>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,24 +5,30 @@ import mobilebg from "../assets/images/bg-tablet-pattern.svg";
 const Hero = () => {
   return (
     <div className="container mx-auto px-4 mt-10">
-      <div className="flex flex-col items-center gap-5">
+      <div className="flex flex-col items-center gap-5 lg:flex-row lg:justify-between">
         <div className="relative">
           <img
             src={charts}
             alt="illustration intro"
-            className="relative z-20"
+            className="relative z-20 select-none lg:w-[650px] 2xl:w-[800px]"
           />
           <img
             src={mobilebg}
             alt="mobile bg"
-            className="absolute left-[15%] -top-[42%] z-10"
+            className="absolute left-[20%] -top-[42%] z-10 select-none md:left-[27%] "
           />
         </div>
-        <div className="flex items-center text-center gap-5 flex-col z-30">
-          <h1 className="font-bold text-4xl max-w-96 text-primary-dark-blue">
+        <div
+          className="flex items-center text-center gap-5 lg:gap-8 flex-col z-30
+          lg:-order-1 lg:text-start lg:items-start"
+        >
+          <h1
+            className="font-bold text-4xl lg:text-5xl 2xl:text-6xl max-w-96 text-primary-dark-blue 
+            lg:max-w-[550px] lg:leading-[1.2]"
+          >
             Bring everyone together to build better products.
           </h1>
-          <p className="text-darkgrayish-blue font-light max-w-[320px] mb-3">
+          <p className="text-darkgrayish-blue font-light leading-[1.6] max-w-[320px] lg:max-w-[350px] mb-3">
             Manage makes it simple for software teams to plan day-to-day tasks
             while keeping the larger team goals in view.
           </p>
